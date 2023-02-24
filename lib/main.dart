c6ef54fe59240266dfe42ba6f24554d5ad3a7521
brucopi <brucopi12@gmail.com>
2023-02-24 10:58:45 -0300

    colocar os aquivos do projeto

---
+++ lib/main.dart
@@ -0,0 +1,338 @@
+import 'dart:async';
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:typed_data/typed_buffers.dart' show Uint8Buffer;
+
+void main() {
+  runApp(
+    meuapp(),
+  );
+}
+
+class meuapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Exemplo MQTT",
+      home: pagina1(title: "Flutter + MQTT"),
+    );
+  }
+}
+
+class pagina1 extends StatelessWidget {
+  pagina1({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      drawer: Drawer(
+        child: menu(),
+      ),
+      body: Container(
+        child: conteudoPagina1(),
+      ),
+    );
+  }
+}
+
+class menu extends StatelessWidget {
+  String end_broker = "";
+  String end_Topic = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FlutterLogo(),
+        Text("Desenvolvido por:"),
+        Text("Bruno Rodrigues"),
+        Icon(
+          Icons.copyright,
+        ),
+        TextField(
+          onChanged: (value) {
+            end_broker = value;
+            print("$end_broker ");
+          },
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            labelText: 'broker - test.mosquitto.org',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        Padding(padding: EdgeInsets.all(5)),
+        TextField(
+          onChanged: (value) {
+            end_Topic = value;
+          },
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            labelText: 'topic - FIT',
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class conteudoPagina1 extends StatefulWidget {
+  const conteudoPagina1({super.key});
+
+  @override
+  State<conteudoPagina1> createState() => _conteudoPagina1State();
+}
+
+String broker = "test.mosquitto.org";
+String topico = "FIT";
+
+class _conteudoPagina1State extends State<conteudoPagina1> {
+  double _temp = 20;
+  bool liga = false;
+//variaveis para conectar ao broker
+
+  final client = MqttServerClient(broker, '');
+
+  Future<int> conectar() async {
+    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
+    /// for details.
+    /// To use websockets add the following lines -:
+    /// client.useWebSocket = true;
+    /// client.port = 80;  ( or whatever your WS port is)
+    /// There is also an alternate websocket implementation for specialist use, see useAlternateWebSocketImplementation
+    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
+    /// You can also supply your own websocket protocol list or disable this feature using the websocketProtocols
+    /// setter, read the API docs for further details here, the vast majority of brokers will support the client default
+    /// list so in most cases you can ignore this.
+    /// Set logging on if needed, defaults to off
+    client.logging(on: true);
+
+    /// Set the correct MQTT protocol for mosquito
+    client.setProtocolV311();
+
+    /// If you intend to use a keep alive you must set it here otherwise keep alive will be disabled.
+    client.keepAlivePeriod = 20;
+
+    /// The connection timeout period can be set if needed, the default is 5 seconds.
+    client.connectTimeoutPeriod = 2000; // milliseconds
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = onDisconnected;
+
+    /// Add the successful connection callback
+    client.onConnected = onConnected;
+
+    /// Add a subscribed callback, there is also an unsubscribed callback if you need it.
+    /// You can add these before connection or change them dynamically after connection if
+    /// you wish. There is also an onSubscribeFail callback for failed subscriptions, these
+    /// can fail either because you have tried to subscribe to an invalid topic or the broker
+    /// rejects the subscribe request.
+    client.onSubscribed = onSubscribed;
+
+    /// Set a ping received callback if needed, called whenever a ping response(pong) is received
+    /// from the broker.
+    client.pongCallback = pong;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password and clean session,
+    /// an example of a specific one below.
+    final connMess = MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId')
+        .withWillTopic(
+            'willtopic') // If you set this you must set a will message
+        .withWillMessage('My Will message')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atLeastOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect();
+    } on NoConnectionException catch (e) {
+      // Raised by the client when connection fails.
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    } on SocketException catch (e) {
+      // Raised by the socket layer
+      print('EXAMPLE::socket exception - $e');
+      client.disconnect();
+    }
+
+    /// Check we are connected
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      /// Use status here rather than state if you also want the broker return code.
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+      exit(-1);
+    }
+
+    /// Ok, lets try a subscription
+    print('EXAMPLE::Subscribing to the test/lol topic');
+    String topic = topico; // Not a wildcard topic
+    client.subscribe(topic, MqttQos.atMostOnce);
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      /// The above may seem a little convoluted for users only interested in the
+      /// payload, some users however may be interested in the received publish message,
+      /// lets not constrain ourselves yet until the package has been in the wild
+      /// for a while.
+      /// The payload is a byte buffer, this will be specific to the topic
+      print(
+          'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+      print('');
+      setState(() {
+        _temp = double.parse(pt);
+      });
+    });
+
+    /// If needed you can listen for published messages that have completed the publishing
+    /// handshake which is Qos dependant. Any message received on this stream has completed its
+    /// publishing handshake with the broker.
+    client.published!.listen((MqttPublishMessage message) {
+      print(
+          'EXAMPLE::Published notification:: topic is ${message.variableHeader!.topicName}, with Qos ${message.header!.qos}');
+    });
+
+    /// Lets publish to our topic
+    /// Use the payload builder rather than a raw buffer
+    /// Our known topic to publish to
+    String pubTopic = topico;
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('Conectado ao APP Flutter MQTT');
+
+    /// Subscribe to it
+    print('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
+    client.subscribe(pubTopic, MqttQos.exactlyOnce);
+
+    /// Publish it
+    print('EXAMPLE::Publishing our topic');
+    client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
+
+    /// Ok, we will now sleep a while, in this gap you will see ping request/response
+    /// messages being exchanged by the keep alive mechanism.
+    print('EXAMPLE::Sleeping....');
+    await MqttUtilities.asyncSleep(60);
+
+    /// Finally, unsubscribe and exit gracefully
+    print('EXAMPLE::Unsubscribing');
+    client.unsubscribe(topic);
+
+    /// Wait for the unsubscribe message from the broker if you wish.
+    await MqttUtilities.asyncSleep(2);
+    print('EXAMPLE::Disconnecting');
+    client.disconnect();
+    print('EXAMPLE::Exiting normally');
+    return 0;
+  }
+
+  /// The subscribed callback
+  void onSubscribed(String topic) {
+    print('EXAMPLE::Subscription confirmed for topic $topic');
+  }
+
+  /// The unsolicited disconnect callback
+  void onDisconnected() {
+    print('EXAMPLE::OnDisconnected client callback - Client disconnection');
+    if (client.connectionStatus!.disconnectionOrigin ==
+        MqttDisconnectionOrigin.solicited) {
+      print('EXAMPLE::OnDisconnected callback is solicited, this is correct');
+    } else {
+      print(
+          'EXAMPLE::OnDisconnected callback is unsolicited or none, this is incorrect - exiting');
+      exit(-1);
+    }
+    if (pongCount == 3) {
+      print('EXAMPLE:: Pong count is correct');
+    } else {
+      print('EXAMPLE:: Pong count is incorrect, expected 3. actual $pongCount');
+    }
+  }
+
+  /// The successful connect callback
+  void onConnected() {
+    print(
+        'EXAMPLE::OnConnected client callback - Client connection was successful');
+  }
+
+  /// Pong callback
+  void pong() {
+    print('EXAMPLE::Ping response client callback invoked');
+    pongCount++;
+  }
+
+  var pongCount = 0; // Pong counter
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Text(
+              "Temperatura º",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(15)),
+            SizedBox(
+              height: 400,
+              child: SfLinearGauge(
+                orientation: LinearGaugeOrientation.vertical,
+                //minimum: -20,
+                maximum: 100,
+                ranges: [
+                  LinearGaugeRange(
+                    color: Colors.lightGreen,
+                    endValue: _temp,
+                    //curva borda
+                    edgeStyle: LinearEdgeStyle.bothCurve,
+                  ),
+                ],
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            Text("Servidor broker MQTT"),
+            Text(broker),
+            Text("Topic= $topico"),
+            Text("Desligado / Ligado"),
+            Switch(
+              value: liga,
+              onChanged: (value) {
+                setState(() {
+                  liga = value;
+                  if (liga) {
+                    conectar();
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
